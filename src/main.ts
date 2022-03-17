5e5af936d32924e663fcd641eed3995b22dd5a1f
Azt3kCode <alexis.aets18@gmail.com>
2022-03-17 15:17:21 -0600

    Avance de Home

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,11 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+ 
+const element: any = document.getElementById('principal_bg');
+
+element.addEventListener('mousemove', (e: any) => {
+  const x = e.clientX / 50;
+  const y = e.clientY / 50;
+  element.style.transform = `translate(${x}px, ${y}px)`;
+});
